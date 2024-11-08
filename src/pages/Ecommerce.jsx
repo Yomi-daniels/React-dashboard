aae6ea106ab5dc339a7005ddc91c9d9c9356cbac
YODA <121935558+Yomi-daniels@users.noreply.github.com>
2024-11-08 23:17:19 +0100

    added changes

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -37,7 +37,7 @@ const Ecommerce = () => {
   return (
     <div className="mt-24 ">
       <div className="flex flex-wrap lg:flex-nowrap justify-center ">
-        <div className="relative dark:text-gray-200 !dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center overflow-hidden bg-white hover:drop-shadow-xl">
+        <div className="relative dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3  bg-no-repeat bg-cover bg-center overflow-hidden bg-white hover:drop-shadow-xl">
           <div className="flex justify-between items-center ">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
